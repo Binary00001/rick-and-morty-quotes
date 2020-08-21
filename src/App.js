390d3e38166f4f9cb5eedc9a28ae664ad094d170
binary00001 <jordansdad253@gmail.com>
2020-08-21 11:58:14 -0700

    added a bunch of stuff

--- src/App.js
+++ src/App.js
@@ -1,21 +1,58 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+import QuoteList from "./components/quoteList";
+
+const App = () => {
+  const [quote, setQuote] = useState({});
+
+  const getQuote = () => {
+    let index = Math.floor(Math.random() * QuoteList.length);
+    setQuote(QuoteList[index]);
+  };
+
+  useEffect(() => {
+    let index = Math.floor(Math.random() * QuoteList.length);
+
+    setQuote(QuoteList[index]);
+  }, []);
+
+  return (
+    <div className="App">
+      <div className="quote-box" id="quote-box">
+        <div className="grey-strip">
+          <p className="title">Rick and Morty Quote Generator</p>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="quote-content">
+          <div className="quote-text" id="text">
+            "{quote.quote}"
+          </div>
+          <div className="quote-author" id="author">
+            - {quote.character}
+          </div>
+          <p>
+            Season: {quote.season}
+            <br />
+            Episode: <em>{quote.episode}</em>
+          </p>
+        </div>
+
+        <div className="links">
+          <a
+            href="#!"
+            className="new-quote btn"
+            id="new-quote"
+            onClick={getQuote}
+          >
+            Generate New Quote
+          </a>
+          {/* <a href="#!" className="tweet-quote btn" id="tweet-quote">
+            Tweet Quote
+          </a> */}
+        </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default App;
---
+++ src/components/quoteList.js
@@ -0,0 +1,558 @@
+const QuoteList = [
+  {
+    character: "Rick Sanchez",
+    quote: "Your parents are a bag of dicks.",
+    season: 4,
+    episode: "Childrick of Mort",
+  },
+  {
+    character: "Summer Smith",
+    quote: "If I die, don't eat my ass that'd be weird",
+    season: 4,
+    episode: "Childrick of Mort",
+  },
+  {
+    character: "Summer Smith",
+    quote: "Camping is just being homeless without the change",
+    season: 4,
+    episode: "Childrick of Mort",
+  },
+  {
+    character: "Rick Sanchez",
+    quote: "Isn't it obvious Morty? I fucked a planet",
+    season: 4,
+    episode: "Childrick of Mort",
+  },
+  {
+    character: "Rick Sanchez",
+    quote: "I wasn't born into the God business, I fucking earned it.",
+    season: 4,
+    episode: "Rattlestar Ricklactica",
+  },
+  {
+    character: "Rick Sanchez",
+    quote: "I programmed you to believe that",
+    season: 4,
+    episode: "One Crew Over the Crewcoo's Morty",
+  },
+  {
+    character: "Morty Smith",
+    quote: "Whose kidneys are these?",
+    season: 4,
+    episode: "One Crew Over the Crewcoo's Morty",
+  },
+  {
+    character: "Rick Sanchez",
+    quote: "Stealing stuff is about the stuff, not the stealing.",
+    season: 4,
+    episode: "One Crew Over the Crewcoo's Morty",
+  },
+  {
+    character: "Summer Smith",
+    quote: "I'm going to daughter your brains out, bitch.",
+    season: 4,
+    episode: "The Old Man and the Seat",
+  },
+  {
+    character: "Jerry Smith",
+    quote: "You can't stay mad at a guy with this amount of talent.",
+    season: 4,
+    episode: "The Old Man and the Seat",
+  },
+  {
+    character: "Morty Smith",
+    quote:
+      "Must... continue... moving... in... ways... that... lead... to... dying... with... you.",
+    season: 4,
+    episode: "Edge of Tomorty",
+  },
+  {
+    character: "Rick Sanchez",
+    quote: "I think you have to think ahead and live in the moment.",
+    season: 4,
+    episode: "Edge of Tomorty",
+  },
+  {
+    character: "Rick Sanchez",
+    quote:
+      "There's a lesson here and I'm not going to be the one to figure it out.",
+    season: 4,
+    episode: "Edge of Tomorty",
+  },
+  {
+    character: "Morty Smith",
+    quote: "I ain't better than shit, Jack.",
+    season: 4,
+    episode: "Edge of Tomorty",
+  },
+  {
+    character: "Rick Sanchez",
+    quote: "Morty, you know outer space is up right?",
+    season: 4,
+    episode: "Edge of Tomorty",
+  },
+  {
+    character: "Rick Sanchez",
+    quote: "Morty, I'm a drunk, not a hack.",
+    season: 3,
+    episode: "Vindicators 3",
+  },
+  {
+    character: "Rick Sanchez",
+    quote: "I've got an emo streak. It's part of what makes me so rad.",
+    season: 2,
+    episode: "Big Trouble in Little Sanchez",
+  },
+  {
+    character: "Rick Sanchez",
+    quote:
+      "Can somebody just let me out of here? If I die in a cage I lose a bet.",
+    season: 2,
+    episode: "A Rickle in Time",
+  },
+  {
+    character: "Morty Smith",
+    quote:
+      "You guys, are the fucking worst! Your gods are a lie! Fuck you, fuck nature and fuck trees!",
+    season: 2,
+    episode: "The Ricks Must Be Crazy",
+  },
+  {
+    character: "Summer Smith",
+    quote: "Ooh yeah, shame me. At least when I'm disgusting it's on purpose.",
+    season: 2,
+    episode: "Total Rickall",
+  },
+  {
+    character: "Summer Smith",
+    quote:
+      "You and Unity are like... like leggings and mid-calf boots. You think you're great together, but you're just bringing out the worst in each other.",
+    season: 2,
+    episode: "Auto Erotic Assimilation",
+  },
+  {
+    character: "Rick Sanchez",
+    quote:
+      "Don't waste your brain on those weridos, Unity. They're no different from any of the aimless chumps that you occupy. They just put you at the center of their lives because you're powerful. And then, because they put you there, they want you to be less powerful. Never gonna happen though, right?",
+    season: 2,
+    episode: "Auto Erotic Assimilation",
+  },
+  {
+    character: "Morty Smith",
+    quote:
+      "Mr. President, if I've learned one thing today, it's that sometimes you have to not give a fuck!",
+    season: 2,
+    episode: "Get Schwifty",
+  },
+  {
+    character: "Rick Sanchez",
+    quote:
+      "Wait for the ramp, Morty. They love the slow ramp. It really gets their dicks hard",
+    season: 2,
+    episode: "The Ricks Must Be Crazy",
+  },
+  {
+    character: "Tammy",
+    quote:
+      "I love watching bukkake. I mean, like, I don't know if I would personally ever do it.",
+    season: 1,
+    episode: "Ricksy Business",
+  },
+  {
+    character: "Rick Sanchez",
+    quote:
+      "Listen, Jerry, I don't want to overstep my bounds or anything. It's your house. It's your world. You're a real Julius Caesar, but I'll tell you something, tell you how-how I feel about school, Jerry. It's a waste of time, a bunch of people running around, bumping into each other. G-guy up front says, 'two plus two'. The people in the back say, 'four'. Then the bell rings, and they give you a carton of milk and a piece of paper that says you can take a dump or something. I mean, it's-it's not a place for smart people, Jerry, and I know that's not a popular opinion, but it's my two cents on the issue.",
+    season: 1,
+    episode: "Pilot",
+  },
+  {
+    character: "Summer Smith",
+    quote:
+      "That's because losers look stuff up while the rest of us are carp'en all them 'diems.",
+    season: 3,
+    episode: "Rickmancing the Stone",
+  },
+  {
+    character: "Rick Sanchez",
+    quote:
+      "Come on, flip the pickle, Morty. You're not gonna regret it. The payoff is huge. I turned myself into a pickle, Morty! Boom! Big reveal! I'm a pickle! What do you think about that? I turned myself inot a pickle! W-what are you just staring at me for, bro? I turned myself into a pickle, Morty.",
+    season: 3,
+    episode: "Pickle Rick",
+  },
+  {
+    character: "Mr. Meeseeks",
+    quote:
+      "Having a family doesn't mean that you stop being an individual. You know the best thing you can do for the people that depend on you? Be honest with them, even if it means setting them free.",
+    season: 1,
+    episode: "Meeseeks and Destroy",
+  },
+  {
+    character: "Rick Sanchez",
+    quote:
+      "Morty, you gotta flip 'em off. I told them it means 'peace among worlds'. How hilarious is that!",
+    season: 2,
+    episode: "The Ricks Must Be Crazy",
+  },
+  {
+    character: "Rick Sanchez",
+    quote: "I can't wait to watch your adventure lay a huge fart.",
+    season: 1,
+    episode: "Meeseeks and Destroy",
+  },
+  {
+    character: "Morty Smith",
+    quote: "I masturbated to an extra curvy piece of driftwood the other day!",
+    season: 2,
+    episode: "The Ricks Must Be Crazy",
+  },
+  {
+    character: "Mr. Meeseeks",
+    quote: "I'm Mr. Meeseeks! Look at me!",
+    season: 1,
+    episode: "Meeseeks and Destroy",
+  },
+  {
+    character: "Morty Jr",
+    quote:
+      "My life has been a lie! God is dead! The government's lame! Thanksgiving is about killing Indians! Jesus wasn't born on Christmas! They moved the date, it was a pagan holiday!",
+    season: 1,
+    episode: "Raising Gazorpazorp",
+  },
+  {
+    character: "Rick Sanchez",
+    quote: "Thanks, Mr. Poopybutthole. I always could count on you.",
+    season: 2,
+    episode: "Total Rickall",
+  },
+  {
+    character: "Rick Sanchez",
+    quote:
+      "I took your family? Who do you think had taken more from them when you shot 20 CCs of liquid dreamkiller into my daughter?",
+    season: 3,
+    episode: "The Whirly Dirly Conspiracy",
+  },
+  {
+    character: "Rick Sanchez",
+    quote:
+      "I know the two of you are very different from each other in a lot of ways, but you have to understand that as far as Grandpa's concerned, you're both pieces of shit! Yeah. I can prove it mathematically. Actually, l-l-let me grab my whiteboard. This has been a long time coming, anyways.",
+    season: 2,
+    episode: "A Rickle in Time",
+  },
+  {
+    character: "Morty Smith",
+    quote:
+      "Well then get your shit together, get it all together and put it in a backpack, all your shit, so it's together. And if you gotta take it somewhere, take it somewhere, you know, take it to the shit store and sell it, or put it in the shit museum. I don't care what you do, you just gotta get it together. Get your shit together.",
+    season: 2,
+    episode: "Big Trouble in Little Sanchez",
+  },
+  {
+    character: "Poncho",
+    quote:
+      "That's right, baby. A lot of people would pay top dollar to decimate the population. I'll take the highest bidder--Al-Qaeda, North Korea, Republicans, shriners, balding men that work out, people on the internet that are only turned on by cartoons of Japanese teenagers. Anything is better than working for you, you pompous, negligent, iTunes-gift-card-as-a-holiday bonus-giving mother...",
+    season: 1,
+    episode: "Anatomy Park",
+  },
+  {
+    character: "Rick Sanchez",
+    quote: "Rubber baby bubby bunkers",
+    season: 2,
+    episode: "Total Rickall",
+  },
+  //   {
+  //     character: "",
+  //     quote: "",
+  //     episode: "",
+  //   },
+  //   {
+  //     character: "",
+  //     quote: "",
+  //     episode: "",
+  //   },
+  //   {
+  //     character: "",
+  //     quote: "",
+  //     episode: "",
+  //   },
+  //   {
+  //     character: "",
+  //     quote: "",
+  //     episode: "",
+  //   },
+  //   {
+  //     character: "",
+  //     quote: "",
+  //     episode: "",
+  //   },
+  //   {
+  //     character: "",
+  //     quote: "",
+  //     episode: "",
+  //   },
+  //   {
+  //     character: "",
+  //     quote: "",
+  //     episode: "",
+  //   },
+  //   {
+  //     character: "",
+  //     quote: "",
+  //     episode: "",
+  //   },
+  //   {
+  //     character: "",
+  //     quote: "",
+  //     episode: "",
+  //   },
+  //   {
+  //     character: "",
+  //     quote: "",
+  //     episode: "",
+  //   },
+  //   {
+  //     character: "",
+  //     quote: "",
+  //     episode: "",
+  //   },
+  //   {
+  //     character: "",
+  //     quote: "",
+  //     episode: "",
+  //   },
+  //   {
+  //     character: "",
+  //     quote: "",
+  //     episode: "",
+  //   },
+  //   {
+  //     character: "",
+  //     quote: "",
+  //     episode: "",
+  //   },
+  //   {
+  //     character: "",
+  //     quote: "",
+  //     episode: "",
+  //   },
+  //   {
+  //     character: "",
+  //     quote: "",
+  //     episode: "",
+  //   },
+  //   {
+  //     character: "",
+  //     quote: "",
+  //     episode: "",
+  //   },
+  //   {
+  //     character: "",
+  //     quote: "",
+  //     episode: "",
+  //   },
+  //   {
+  //     character: "",
+  //     quote: "",
+  //     episode: "",
+  //   },
+  //   {
+  //     character: "",
+  //     quote: "",
+  //     episode: "",
+  //   },
+  //   {
+  //     character: "",
+  //     quote: "",
+  //     episode: "",
+  //   },
+  //   {
+  //     character: "",
+  //     quote: "",
+  //     episode: "",
+  //   },
+  //   {
+  //     character: "",
+  //     quote: "",
+  //     episode: "",
+  //   },
+  //   {
+  //     character: "",
+  //     quote: "",
+  //     episode: "",
+  //   },
+  //   {
+  //     character: "",
+  //     quote: "",
+  //     episode: "",
+  //   },
+  //   {
+  //     character: "",
+  //     quote: "",
+  //     episode: "",
+  //   },
+  //   {
+  //     character: "",
+  //     quote: "",
+  //     episode: "",
+  //   },
+  //   {
+  //     character: "",
+  //     quote: "",
+  //     episode: "",
+  //   },
+  //   {
+  //     character: "",
+  //     quote: "",
+  //     episode: "",
+  //   },
+  //   {
+  //     character: "",
+  //     quote: "",
+  //     episode: "",
+  //   },
+  //   {
+  //     character: "",
+  //     quote: "",
+  //     episode: "",
+  //   },
+  //   {
+  //     character: "",
+  //     quote: "",
+  //     episode: "",
+  //   },
+  //   {
+  //     character: "",
+  //     quote: "",
+  //     episode: "",
+  //   },
+  //   {
+  //     character: "",
+  //     quote: "",
+  //     episode: "",
+  //   },
+  //   {
+  //     character: "",
+  //     quote: "",
+  //     episode: "",
+  //   },
+  //   {
+  //     character: "",
+  //     quote: "",
+  //     episode: "",
+  //   },
+  //   {
+  //     character: "",
+  //     quote: "",
+  //     episode: "",
+  //   },
+  //   {
+  //     character: "",
+  //     quote: "",
+  //     episode: "",
+  //   },
+  //   {
+  //     character: "",
+  //     quote: "",
+  //     episode: "",
+  //   },
+  //   {
+  //     character: "",
+  //     quote: "",
+  //     episode: "",
+  //   },
+  //   {
+  //     character: "",
+  //     quote: "",
+  //     episode: "",
+  //   },
+  //   {
+  //     character: "",
+  //     quote: "",
+  //     episode: "",
+  //   },
+  //   {
+  //     character: "",
+  //     quote: "",
+  //     episode: "",
+  //   },
+  //   {
+  //     character: "",
+  //     quote: "",
+  //     episode: "",
+  //   },
+  //   {
+  //     character: "",
+  //     quote: "",
+  //     episode: "",
+  //   },
+  //   {
+  //     character: "",
+  //     quote: "",
+  //     episode: "",
+  //   },
+  //   {
+  //     character: "",
+  //     quote: "",
+  //     episode: "",
+  //   },
+  //   {
+  //     character: "",
+  //     quote: "",
+  //     episode: "",
+  //   },
+  //   {
+  //     character: "",
+  //     quote: "",
+  //     episode: "",
+  //   },
+  //   {
+  //     character: "",
+  //     quote: "",
+  //     episode: "",
+  //   },
+  //   {
+  //     character: "",
+  //     quote: "",
+  //     episode: "",
+  //   },
+  //   {
+  //     character: "",
+  //     quote: "",
+  //     episode: "",
+  //   },
+  //   {
+  //     character: "",
+  //     quote: "",
+  //     episode: "",
+  //   },
+  //   {
+  //     character: "",
+  //     quote: "",
+  //     episode: "",
+  //   },
+  //   {
+  //     character: "",
+  //     quote: "",
+  //     episode: "",
+  //   },
+  //   {
+  //     character: "",
+  //     quote: "",
+  //     episode: "",
+  //   },
+  //   {
+  //     character: "",
+  //     quote: "",
+  //     episode: "",
+  //   },
+  //   {
+  //     character: "",
+  //     quote: "",
+  //     episode: "",
+  //   },
+  //   {
+  //     character: "",
+  //     quote: "",
+  //     episode: "",
+  //   },
+];
+
+export default QuoteList;
